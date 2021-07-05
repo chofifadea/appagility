5c33682558b55d652c9c847cdf696019e8ca4540
Chofifa Dea <chofifad@gmail.com>
2021-07-05 12:32:03 +0700

    menambahkan file configurasi deployment .cpanel.yml

--- app/Views/admin/inbox.php
+++ app/Views/admin/inbox.php
@@ -8,7 +8,7 @@
     <h1 class="h3 mb-2 text-gray-800">Inbox Data Pallet</h1>
 
     <div style="" class="row">
-        <?php if(count($rows) == 0): ?>
+        <?php if (count($rows) == 0) : ?>
             <div class="col-xl-3 col-lg-4 col-md-6 col-12">
                 <div class="card">
                     <div class="card-body">
@@ -17,20 +17,20 @@
                 </div>
             </div>
         <?php endif ?>
-        <?php foreach($rows as $row): ?>
+        <?php foreach ($rows as $row) : ?>
             <!-- Notifikasi Inbox-->
             <div class="col-12 col-xl-3 col-lg-4 col-md-6 mb-10 inbox-col" style="margin-bottom: 1rem;">
                 <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="false" data-delay="100000">
                     <div class="toast-body">
                         <div style="min-height: 70px;">
-                            Masuk Palet <b><?= $row['nama_pallet'] ?></b> dari Site <b><?= $row['nama_site_asal'] ?></b> 
-                            <?= $is_superadmin ? 'ke Site <b>' . $row['nama_site_tujuan'] . '</b>': '' ?> 
+                            Masuk Palet <b><?= $row['nama_pallet'] ?></b> dari Site <b><?= $row['nama_site_asal'] ?></b>
+                            <?= $is_superadmin ? 'ke Site <b>' . $row['nama_site_tujuan'] . '</b>' : '' ?>
                             dengan jumlah <b><?= $row['quantity'] ?></b>
-                            
+
                         </div>
                         <small><?= date('d M Y H:i', strtotime($row['created_at'])) ?></small>
                         <div class="mt-2 pt-2 border-top">
-                            <button type="button" class="btn btn-primary btn-sm btn-approve" data-id="<?= $row['id'] ?>">Take action</button>
+                            <button type="button" class="btn btn-primary btn-sm btn-approve" data-id="<?= $row['id'] ?>">Confirm</button>
                             <button type="button" class="btn btn-secondary btn-sm btn-reject" data-bs-decline="toast" data-id="<?= $row['id'] ?>">Decline</button>
                         </div>
                     </div>
@@ -38,11 +38,11 @@
             </div>
         <?php endforeach ?>
     </div>
-    
+
 </div>
 
 <?= $this->endSection(); ?>
 
 <?= $this->section('page_js') ?>
-    <script type="text/javascript" src="<?= base_url() ?>/js/page-inbox-1.js?t=11"></script>
+<script type="text/javascript" src="<?= base_url() ?>/js/page-inbox-1.js?t=11"></script>
 <?= $this->endSection() ?>
